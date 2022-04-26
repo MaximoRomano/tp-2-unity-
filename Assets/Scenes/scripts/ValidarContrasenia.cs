1adbcfdd37fd65f3eacd5c31f0604b460d94ffc3
MaximoRomano <102158982+MaximoRomano@users.noreply.github.com>
2022-04-25 22:26:58 -0300

    password with debug completed

---
+++ Assets/Scenes/scripts/ValidarContrasenia.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasenia : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    string contraseniausuario;
+    public Text ingresousuario;
+
+    void Start()
+    {
+        contraseniaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasenia()
+    {
+        contraseniausuario = ingresousuario.text;
+        if(contraseniausuario == contraseniaCorrecta)
+        {
+            Debug.Log("bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
+    }
+}
